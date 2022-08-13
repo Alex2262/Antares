0342e42674ffea4dbaaf1b1288fc1d91fd28aeb1
Alexander Tian <alex2262alexander@gmail.com>
2022-08-13 14:23:22 -0400

    Bug fixes

--- search.py
+++ search.py
@@ -36,7 +36,6 @@ def reset_search(engine):
 
     engine.killer_moves = np.zeros((2, engine.max_depth), dtype=np.uint32)
     engine.history_moves = np.zeros((12, 64), dtype=np.uint32)
-    engine.transposition_table = np.zeros(MAX_HASH_SIZE, dtype=NUMBA_HASH_TYPE)
 
     engine.stopped = False
 
@@ -50,7 +49,8 @@ def new_game(engine):
 
 
 @nb.njit(cache=True)
-def get_pv_line(engine, position):
+def get_pv_line(engine, position, depth):
+
     pv_line = []
 
     entry = engine.transposition_table[position.hash_key % MAX_HASH_SIZE]
@@ -61,7 +61,15 @@ def get_pv_line(engine, position):
     if entry.move == NO_MOVE:
         return pv_line
 
+    in_check = is_attacked(position, position.king_positions[position.side])
+    if in_check:
+        depth += 1
+
     pv_line.append(get_uci_from_move(entry.move))
+
+    # This ensures repetitions and stuff won't cause infinite loops.
+    if depth != entry.depth:
+        return pv_line
 
     current_ep = position.ep_square
     current_hash_key = position.hash_key
@@ -70,7 +78,7 @@ def get_pv_line(engine, position):
     make_move(position, entry.move)
     position.side ^= 1
 
-    next_moves = get_pv_line(engine, position)
+    next_moves = get_pv_line(engine, position, depth - 1)
 
     position.side ^= 1
     undo_move(position, entry.move, current_ep, current_castle_ability_bits, current_hash_key)
@@ -179,6 +187,13 @@ def negamax(engine, position, alpha, beta, depth):
     if engine.stopped:
         return 0
 
+    in_check = is_attacked(position,
+                           position.king_positions[position.side])
+
+    # Check extension
+    if in_check:
+        depth += 1
+
     # Get a value from probe_tt_entry that will correspond to either returning a score immediately
     # or returning no hash entry, or returning move int to sort
     tt_value = probe_tt_entry(engine, position, alpha, beta, depth)
@@ -197,12 +212,6 @@ def negamax(engine, position, alpha, beta, depth):
 
     # Used at the end of the negamax function to determine checkmates, stalemates, etc.
     legal_moves = 0
-    in_check = is_attacked(position,
-                           position.king_positions[position.side])
-
-    # Check extension
-    if in_check:
-        depth += 1
 
     # Saving information to undo moves successfully.
     current_ep = position.ep_square
@@ -343,7 +352,7 @@ def negamax(engine, position, alpha, beta, depth):
     elif legal_moves == 0 and in_check:
         return -MATE_SCORE - depth
 
-    if -MATE_SCORE < alpha < MATE_SCORE:
+    if -MATE_SCORE < alpha < MATE_SCORE and not engine.stopped:
         record_tt_entry(engine, position, alpha, tt_hash_flag, best_move, depth)
 
     # We return our best score possible. This is an 'All node' and we have failed low
@@ -391,7 +400,7 @@ def iterative_search(engine, position, compiling):
         beta = returned + ASPIRATION_VAL
 
         # Obtain principle variation line and print it
-        pv_line = get_pv_line(engine, position)
+        pv_line = get_pv_line(engine, position, running_depth)
 
         position.side = original_side
 
@@ -1146,20 +1155,20 @@ info depth 13 score cp 29 time 7342 nodes 6417711 nps 874044 pv e2e4 b8c6 g1f3 e
 info depth 13 score cp 29 time 60000 nodes 49319159 nps 821973 pv e2e4 b8c6 g1f3 e7e6 d2d4 d7d5 e4e5 g8e7 b1c3
 bestmove e2e4
 
-King eval
-info depth 1 score cp 42 time 5 nodes 21 nps 3590 pv b1c3
-info depth 2 score cp 8 time 6 nodes 124 nps 19764 pv g1f3 b8c6
-info depth 3 score cp 42 time 6 nodes 260 nps 40277 pv g1f3 b8c6 b1c3
-info depth 4 score cp 6 time 8 nodes 1276 nps 155701 pv g1f3 b8c6 b1c3 e7e5
-info depth 5 score cp 36 time 10 nodes 2758 nps 269572 pv g1f3 b8c6 b1c3 e7e5 e2e4
-info depth 6 score cp 2 time 27 nodes 16144 nps 593462 pv g1f3 g8f6 b1c3 b8c6 e2e4 e7e5
-info depth 7 score cp 16 time 38 nodes 25615 nps 666016 pv g1f3 g8f6 b1c3 b8c6 d2d4 d7d5 e2e3
-info depth 8 score cp 14 time 107 nodes 81164 nps 753591 pv b1c3 g8f6 e2e4 b8c6 g1f3 d7d5 e4e5 f6g4
-info depth 9 score cp 25 time 477 nodes 383261 nps 803298 pv e2e4 b8c6 d2d4 d7d5 e4d5 d8d5 g1e2 g8f6 b1c3
-info depth 10 score cp 12 time 925 nodes 749260 nps 809278 pv e2e4 d7d5 e4d5
-info depth 11 score cp 26 time 1718 nodes 1410601 nps 820653 pv e2e4 d7d5 e4d5 g8f6 g1f3 f6d5 d2d4 d8d6 c2c4 d5f4 b1c3
-info depth 12 score cp 32 time 3389 nodes 2796542 nps 825158 pv e2e4 d7d5 e4d5 g8f6 g1f3 f6d5
-info depth 13 score cp 29 time 19502 nodes 15850795 nps 812738 pv e2e4 e7e6 g1f3 b8c6 f1b5 g8f6 b1c3 a7a6 d2d4 a6b5 e1g1
-info depth 13 score cp 29 time 60001 nodes 47551635 nps 792510 pv e2e4 e7e6 g1f3 b8c6 f1b5 g8f6 b1c3 a7a6 d2d4 a6b5 e1g1
+King eval + bug fix
+info depth 1 score cp 42 time 25 nodes 21 nps 831 pv b1c3
+info depth 2 score cp 8 time 25 nodes 124 nps 4806 pv g1f3 b8c6
+info depth 3 score cp 42 time 25 nodes 260 nps 10002 pv g1f3 b8c6 b1c3
+info depth 4 score cp 6 time 27 nodes 1276 nps 45876 pv g1f3 b8c6 b1c3 e7e5
+info depth 5 score cp 36 time 30 nodes 2758 nps 91875 pv g1f3 b8c6 b1c3 e7e5 e2e4
+info depth 6 score cp 2 time 48 nodes 16144 nps 330759 pv g1f3 g8f6 b1c3 b8c6 e2e4 e7e5
+info depth 7 score cp 16 time 61 nodes 25615 nps 419864 pv g1f3 g8f6 b1c3 b8c6 d2d4 d7d5 e2e3
+info depth 8 score cp 14 time 138 nodes 81305 nps 585747 pv b1c3 g8f6 e2e4 b8c6 g1f3 d7d5 e4e5 f6g4
+info depth 9 score cp 20 time 470 nodes 343014 nps 728583 pv e2e4 b8c6 g1f3 d7d5 b1c3 d5e4 c3e4 g8f6 f3g5
+info depth 10 score cp 12 time 1120 nodes 851382 nps 760089 pv e2e4 d7d5 e4d5
+info depth 11 score cp 26 time 1772 nodes 1372257 nps 774134 pv e2e4 d7d5 e4d5 g8f6 g1f3 f6d5 d2d4 d8d6 c2c4 d5f4 b1c3
+info depth 12 score cp 32 time 3776 nodes 2980183 nps 789071 pv e2e4 d7d5 e4d5 g8f6 g1f3 f6d5
+info depth 13 score cp 34 time 35258 nodes 27360330 nps 775990 pv e2e4 e7e6
+info depth 13 score cp 34 time 60000 nodes 46406859 nps 773438 pv e2e4 e7e6
 bestmove e2e4
 '''
